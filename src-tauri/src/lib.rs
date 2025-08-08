ea8b3ef3695f2ae861d5d0d6e889eeac2b8fe83a
四季柚子 <loremmlel@163.com>
2025-08-08 17:07:46 +0800

    feat(udp): 添加UDP监听功能以发现服务并处理广播消息

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri::{AppHandle, Emitter};
+
 use crate::commands::transcode::{start_transcode, stop_transcode};
 use crate::commands::{
     config::{get_app_config, set_app_config},
@@ -19,6 +21,13 @@ mod utils;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let app_handle = app.handle().clone();
+            tauri::async_runtime::spawn(async move {
+                start_udp_listener(&app_handle).await;
+            });
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
             get_app_config,
@@ -41,4 +50,44 @@ pub fn run() {
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+const BROADCAST_PORT: u16 = 23333;
+const HANDSHAKE_MESSAGE: &str = "SHIKIYUZU CIALLO";
+
+#[derive(Clone, serde::Serialize)]
+struct DiscoveredServicePayload {
+    ip: String,
+}
+
+async fn start_udp_listener(app: &AppHandle) {
+    let listen_address = format!("0.0.0.0:{}", BROADCAST_PORT);
+    match tokio::net::UdpSocket::bind(&listen_address).await {
+        Ok(socket) => {
+            println!("Listening for UDP broadcasts on {}", listen_address);
+            let mut buf = [0; 1024];
+
+            loop {
+                if let Ok((len, _src_addr)) = socket.recv_from(&mut buf).await {
+                    if let Ok(message) = std::str::from_utf8(&buf[..len]) {
+                        if let Some((handshake, ip)) = message.split_once(":") {
+                            if handshake == HANDSHAKE_MESSAGE {
+                                println!("Discovered server at IP: {}", ip);
+                                app.emit(
+                                    "service-discovered",
+                                    DiscoveredServicePayload { ip: ip.to_string() },
+                                )
+                                .unwrap();
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        Err(e) => {
+            eprintln!("Failed to bind UDP socket on {}: {}", listen_address, e);
+            return;
+        }
+    }
 }
