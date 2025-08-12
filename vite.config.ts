bbfc53ea5b88f88870539fdc7e61a74c13a0054a
四季柚子 <loremmlel@163.com>
2025-08-12 10:29:33 +0800

    feat: 更新TypeScript和Vite配置，提升编译目标至ES2022

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,10 @@ const host = process.env.TAURI_DEV_HOST;
 export default defineConfig(async () => ({
   plugins: [react(), tailwindcss()],
 
+  build: {
+    target: 'es2022'
+  },
+
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
