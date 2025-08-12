158c6549466e03f5054ab1fe61f64266e13e48b6
四季柚子 <loremmlel@163.com>
2025-08-12 16:54:20 +0800

    feat: 添加在线和离线收藏夹的逻辑判断，优化获取和操作收藏夹的功能

--- src-tauri/src/commands/favorites.rs
+++ src-tauri/src/commands/favorites.rs
@@ -1,16 +1,22 @@
 use tauri::command;
 
+use crate::commands::config::is_online;
 use crate::models::error::ApiError;
 use crate::models::favorite::{
     AddFileToFavoriteRequest, CreateFavoriteRequest, FavoriteDto, FavoriteFileDto,
 };
 use crate::repos::favorites_repo::FavoritesRepo;
+use crate::repos::offline::offline_favorites_repo::OfflineFavoritesRepo;
 use crate::repos::online::online_favorites_repo::OnlineFavoritesRepo;
 use crate::repos::Repo;
 
 #[command]
 pub async fn get_favorites() -> Result<Vec<FavoriteDto>, ApiError> {
-    OnlineFavoritesRepo::get_all().await
+    if is_online() {
+        OnlineFavoritesRepo::get_all().await
+    } else {
+        OfflineFavoritesRepo::get_all().await
+    }
 }
 
 #[command]
@@ -18,25 +24,45 @@ pub async fn add_file_to_favorite(
     request: AddFileToFavoriteRequest,
     favorite_id: i64,
 ) -> Result<bool, ApiError> {
-    OnlineFavoritesRepo::add_file_to_favorite(request, favorite_id).await
+    if is_online() {
+        OnlineFavoritesRepo::add_file_to_favorite(request, favorite_id).await
+    } else {
+        OfflineFavoritesRepo::add_file_to_favorite(request, favorite_id).await
+    }
 }
 
 #[command]
 pub async fn get_all_favorite_files() -> Result<Vec<FavoriteFileDto>, ApiError> {
-    OnlineFavoritesRepo::get_all_favorite_files().await
+    if is_online() {
+        OnlineFavoritesRepo::get_all_favorite_files().await
+    } else {
+        OfflineFavoritesRepo::get_all_favorite_files().await
+    }
 }
 
 #[command]
 pub async fn delete_favorite_file(id: i64) -> Result<bool, ApiError> {
-    OnlineFavoritesRepo::delete_favorite_file(id).await
+    if is_online() {
+        OnlineFavoritesRepo::delete_favorite_file(id).await
+    } else {
+        OfflineFavoritesRepo::delete_favorite_file(id).await
+    }
 }
 
 #[command]
 pub async fn create_favorite(request: CreateFavoriteRequest) -> Result<FavoriteDto, ApiError> {
-    OnlineFavoritesRepo::create(request).await
+    if is_online() {
+        OnlineFavoritesRepo::create(request).await
+    } else {
+        OfflineFavoritesRepo::create(request).await
+    }
 }
 
 #[command]
 pub async fn delete_favorite(id: i64) -> Result<bool, ApiError> {
-    OnlineFavoritesRepo::delete(id).await
+    if is_online() {
+        OnlineFavoritesRepo::delete(id).await
+    } else {
+        OfflineFavoritesRepo::delete(id).await
+    }
 }
